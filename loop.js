0b4b06f633237e071c75b6f5963c7f4e97a457d7
satani priti <pritisatani@gmail.com>
2025-03-28 11:43:13 +0530

    loop 28-03-25

---
+++ loop.js
@@ -0,0 +1,84 @@
+//1.print 1 to 5
+
+// for(let i=1; i<=5; i++){
+//     console.log(i)
+// }
+
+//2.print even number
+
+// for(let i=2; i<=20; i=i+2){
+//     console.log(i)
+// }
+
+// for(let i=1 ; i<=20; i++){
+//     if(i % 2==0){
+//         console.log(i)
+//     }
+// }
+
+//3.Print out 10 to 1
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// 4. Print out 51 to 99
+// for(let i=51; i<=99; i++){
+//         console.log(i)
+//     }
+    
+// 5. Enter n and print out 1 to n
+// let n;
+// n = prompt("Enter a number:")
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);
+// }
+
+// 6. Enter n and print out n to 1
+// let n;
+// n = prompt("Enter a number:")
+// for (let i = n; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// 5. Enter n and print out -n to n
+// let n = prompt("Enter a number:")
+
+// for (let i = -n; i <= n; i++) {
+//     console.log(i);
+// }
+
+// 6. Enter n and print out odd no. up to n
+// let n = prompt("Enter a number:")
+
+// for (let i = 1; i <= n; i ++) {
+//     if (i % 2 != 0){
+//     console.log(i);
+// }
+// }
+
+// 7. Enter n and print out even no. up to n
+// let n = prompt("Enter a number:")
+
+// for (let i = 2; i <= n; i=i+2) {
+//     console.log(i);
+// }
+
+
+// 8. Enter n and print out table of n up to n*10
+// let n = prompt("Enter a number:"); // Take input from user
+
+// console.log(`Multiplication Table of ${n}:`);
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${n} x ${i} = ${n * i}`);
+// }
+
+
+//9.Enter n and print out table of n up to n*5
+// let n = prompt("Enter a number:"); // Take input from user
+
+// console.log(`Multiplication Table of ${n}:`);
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${n} x ${i} = ${n * i}`);
+// }
