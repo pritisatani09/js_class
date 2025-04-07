fc31b06d989656781e3c90e99b781c074017d61d
satani priti <pritisatani@gmail.com>
2025-04-07 18:19:55 +0530

    7-04-25

--- function_type.js
+++ function_type.js
@@ -1,49 +1,108 @@
-//switch program
-// TNRN 
-function add1(){
-    let a=15,b=25;
-    console.log("Sum of A and B is ",a+b);
-}
+//********************************************** 1.switch program
+// TNRN
+// function add1(){
+//     let a=15,b=25;
+//     console.log("Sum of A and B is ",a+b);
+// }
 
-// TNRS 
-function add2(){
-    let a=15,b=35;
-    let x;
-    x = a + b;
-    return x;
-}
+// // TNRS 
+// function add2(){
+//     let a=15,b=35;
+//     let x;
+//     x = a + b;
+//     return x;
+// }
 
-// TSRN 
-function add3(a,b){
-    let total;
+// // TSRN 
+// function add3(a,b){
+//     let total;
 
-    total = a+b;
-    console.log(total);
-}
+//     total = a+b;
+//     console.log(total);
+// }
 
 
-// TSRS 
-function add4(a,b){
-let total=a+b;
-return total;
-}
+// // TSRS 
+// function add4(a,b){
+// let total=a+b;
+// return total;
+// }
 
-choice = prompt("Enter your Choice:");
+// choice = prompt("Enter your Choice:");
 
-switch(choice){
-case "1":
-    add1();
-    break;
-case "2":
-    console.log(add2());
-    break;
-case "3":
-    add3(10,20);
-    break;
-case "4":
-    console.log(add4(15,20));
-    break;
-default:
-    console.log("Invalid choice");
+// switch(choice){
+// case "1":
+//     add1();
+//     break;
+// case "2":
+//     console.log(add2());
+//     break;
+// case "3":
+//     add3(10,20);
+//     break;
+// case "4":
+//     console.log(add4(15,20));
+//     break;
+// default:
+//     console.log("Invalid choice");
 
-}
+// }
+
+
+//**********************************************2.recursion function
+// function test(n) {
+//     console.log(" inside function:", n);
+//     if (n < 5) {
+//         n++;
+//         test(n);
+//     }
+//     console.log(" outside function:", n);
+// }
+
+// test(1);
+
+//factorial number
+// function factorial(n) {
+//     if (n == 1) {
+//         return 1;
+//         }
+//         else {
+//             return n * factorial(n - 1);
+//             }
+//             }
+//             console.log(factorial(5));
+
+//fibonnaci series
+// function fiboSeries(n) {
+//     let a = 1, b = 1, next;
+//     console.log("Fibonacci Series up to " + n + " terms:");
+//     for (let i = 1; i < n; i++) {
+//         console.log(a);
+//         next = a + b;
+//         a = b;
+//         b = next;
+//     }
+// }
+// fiboSeries(10);
+
+// function fibonacci(n) {
+
+//     if (n === 0) {
+//         return 0;
+//     } else if (n === 1) {
+//         return 1;
+//     } else {
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+    
+// }
+
+// console.log("Fibonacci sequence:");
+// n=10;
+// for (let i = 1; i < n; i++) {
+//     console.log(fibonacci(i));
+// }
+
+
+
+            
---
+++ new_function.js
@@ -0,0 +1,73 @@
+//********** */ 3.variable function,arrow function,nested function,IIFE,clouser function,nested function,callback function
+// nested function
+// function greet (){
+//     console.log("hello....");
+//     function hello (){
+//         console.log("nested function")
+//     }
+//     hello()
+//     console.log("this is nested funcion")
+// }
+// greet()
+
+
+//variable function 
+// variable as a function 
+// let a = function(){
+//     console.log("variable as a function");
+// }
+// a()
+
+
+// arrow function 
+// let add = () =>{
+//     console.log("arrow function");
+//     console.log(10+20);
+// }
+// add()
+
+// or
+
+// let added = () => console.log(" hellooooooooooooooooooooo")
+// added()
+
+
+//clouesr function
+// function greet(){
+//     console.log("Good Evening");
+
+//     function hello(){
+//         console.log("Hello");
+//     }
+//     return hello;
+// }
+// let a = greet();
+// a();
+
+//or
+
+// greet ()();
+
+
+// callback function
+// function test (callback){
+//     console.log(" hieeeeeeeeeeeeeee")
+//     callback()
+//     }
+//     function heello(){
+//         console.log("callback function");
+//     }
+//     test(heello)
+
+
+//IIFE
+// (function(a) {
+//     console.log("IIFE runs immediately!",a);
+//   })(123);
+  
+
+
+
+
+
+
